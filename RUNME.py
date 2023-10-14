b3c46cebf9a09cf7ae063cb28db5f06f151427b5
hoanghai1803 <hoanghai123098@gmail.com>
2023-10-14 17:49:39 +0700

    Get SGINATURE when run

--- RUNME.py
+++ RUNME.py
@@ -1,9 +1,8 @@
 from src.audio_processing import generate_data
 from src.training import do_research
 
-# Change the signature for your experiment
 # The data will be exported after processing and imported when training from the folder with this name
-SIGNATURE = 'ENTER_YOUR_SIGNATURE_HERE'
+SIGNATURE = input('Enter signature: ')
 
 do_generate_data = input("Enter 'YES' to generate data: ")
 if do_generate_data == 'YES':
