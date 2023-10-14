51cc217b40390c26e482b9edaedf223685641a82
hoanghai1803 <hoanghai123098@gmail.com>
2023-10-14 18:31:36 +0700

    Fix bug: cannot enter multiple value in jupyter notebook

--- RUNME.py
+++ RUNME.py
@@ -1,11 +1,18 @@
+import sys
 from src.audio_processing import generate_data
 from src.training import do_research
 
+# Change the signature for your experiment
 # The data will be exported after processing and imported when training from the folder with this name
-SIGNATURE = input('Enter signature: ')
-print(f"Your signature: {SIGNATURE}")
+SIGNATURE = 'ENTER_YOUR_SIGNATURE_HERE'
 
 do_generate_data = input('Enter "YES" to generate data: ')
+
+# You can also enter signature and do_generate_data as command line arguments
+if len(sys.argv) > 2:
+    SIGNATURE = sys.argv[2]
+    do_generate_data = sys.argv[3]
+
 if do_generate_data == 'YES':
     generate_data(signature=SIGNATURE)
 
