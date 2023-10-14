670149eba351505c437a9fbfec2d7e974785d8c6
hoanghai1803 <hoanghai123098@gmail.com>
2023-10-14 18:35:03 +0700

    Fix bug: cannot enter multiple value in jupyter notebook

--- RUNME.py
+++ RUNME.py
@@ -6,12 +6,15 @@ from src.training import do_research
 # The data will be exported after processing and imported when training from the folder with this name
 SIGNATURE = 'ENTER_YOUR_SIGNATURE_HERE'
 
-do_generate_data = input('Enter "YES" to generate data: ')
-
-# You can also enter signature and do_generate_data as command line arguments
-if len(sys.argv) > 2:
+# You can also enter signature as command line argument
+if len(sys.argv) > 1:
     SIGNATURE = sys.argv[2]
+
+# You can enter your generate option as command line argument or input
+if len(sys.argv) > 2:
     do_generate_data = sys.argv[3]
+else:
+    do_generate_data = input('Enter "YES" to generate data: ')
 
 if do_generate_data == 'YES':
     generate_data(signature=SIGNATURE)
