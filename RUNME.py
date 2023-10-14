0bf3b89c9e45f25381eb1e918caa071fe8b1023f
hoanghai1803 <hoanghai123098@gmail.com>
2023-10-14 18:06:45 +0700

    Fix bugs

--- RUNME.py
+++ RUNME.py
@@ -2,9 +2,9 @@ from src.audio_processing import generate_data
 from src.training import do_research
 
 # The data will be exported after processing and imported when training from the folder with this name
-SIGNATURE = input('Enter signature: ')
+SIGNATURE = input('Enter signature: ').strip()
 
-do_generate_data = input("Enter 'YES' to generate data: ")
+do_generate_data = input('Enter "YES" to generate data: ').strip()
 if do_generate_data == 'YES':
     generate_data(signature=SIGNATURE)
 
