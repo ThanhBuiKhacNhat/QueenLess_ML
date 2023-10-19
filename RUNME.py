ba46026aab22d3902f311b5d0fcb81582f26524f
hoanghai1803 <hoanghai123098@gmail.com>
2023-10-20 02:00:31 +0700

    Update RUNME.py

--- RUNME.py
+++ RUNME.py
@@ -1,4 +1,3 @@
-import sys
 from src.audio_processing import generate_data
 from src.training import do_research
 
@@ -7,7 +6,7 @@ from src.training import do_research
 
 # Change the signature for your experiment
 # The data will be exported after processing and imported when training from the folder with this name
-SIGNATURE = 'non_overlapped_2s'
+SIGNATURE = 'ENTER_YOUR_SIGNATURE_HERE'
 
 # True for generate new data, False for use existing data
 GEN_DATA = False
@@ -16,12 +15,12 @@ GEN_DATA = False
 SEGMENT_LENGTH = 2
 
 # True for using overlapped segments, False for using non-overlapped segments
-OVERLAP = True
+OVERLAP = False
 
 # Dataset options: [d_mean, d_std, d_merged, s_mean, s_std, s_merged]
 DATASET = 's_merged'
 
-# END #
+# END IMPORTANT ZONE #
 
 
 if GEN_DATA:
